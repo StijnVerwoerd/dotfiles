8fe91a422b8c8e63382e813167b717c3de8919d5
stijn <stijn.verwoerd@gmail.com>
2025-09-26 15:39:30 +0200

    Added telescope undo & tiny diagnostics

--- nvim/.config/nvim/lua/custom/keybinds.lua
+++ nvim/.config/nvim/lua/custom/keybinds.lua
@@ -2,11 +2,11 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
--- Map <leader> to :ZenMode
+-- Map <leader> + z to :ZenMode
 map('n', '<leader>z', '<cmd>ZenMode<CR>', opts)
 
--- Map <leader> to open the LSP diagnostics on that line
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostics under cursor' })
+-- Map <leader> + n to open the LSP diagnostics on that line
+map('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostics under cursor' })
 
 -- Map 'Enter' to open a new empty line below
-vim.keymap.set('n', '<CR>', 'o<Esc>', { noremap = true, silent = true })
+map('n', '<CR>', 'o<Esc>', opts)
